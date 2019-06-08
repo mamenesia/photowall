ff27298ca5bda7f4321786e8e7433f4961eb6d83
mamenesia <dedy.prasetyo.h@gmail.com>
2019-06-08 21:32:23 +0700

    setting up the App

---
+++ src/Components/Photo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Photo extends Component {
+  render() {
+    const post = this.props.post;
+    return <figure className='figuere'>
+
+    </figure>
+  }
+}
+
+export default Photo;
